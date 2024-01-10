00694507c14be31ccf89c5decad520ecd4f312cf
Guillaume-Panighetti <150042209+Guillaume-Panighetti@users.noreply.github.com>
2024-01-10 18:58:42 +0100

    projet final

---
+++ corpus.py
@@ -0,0 +1,55 @@
+import re
+import pandas as pd
+class Corpus:
+    instance = None
+    def __init__(self, name, authors, id2doc):
+        self.name = name
+        self.authors = authors
+        self.id2doc = id2doc
+        self.ndoc = len(self.id2doc)
+        self.naut = len(self.authors)
+        self.text = ""
+        
+    def setText(self, text):
+        self.text = text
+    def getText(self):
+        return self.text
+
+    def describe(self):
+        print(f"Corpus : {self.name}\t# documents {self.ndoc}\t#auteurs : {self.naut}\t#caracteres : {len(self.text)}")
+        for i, doc in self.id2doc.items():
+            doc = doc.describe()
+            print(f"Document {i}\t# caracteres : {doc['nchar']}\t# mots : {doc['nword']} \t# phrases : {doc['nsent']}\t# type : {doc['type']}")
+    
+    def getDocs(self, sorted=False, byDate=True):
+        newlist = list(self.id2doc.values())
+        if(sorted):
+            if (byDate):    
+                newlist = sorted(newlist, key=lambda x: x.date, reverse=True)
+            else:
+                newlist = sorted(newlist, key=lambda x: x.titre, reverse=False)
+        return newlist
+        
+    def concorde(self, clef, contexte):
+        txt = self.getText()
+        p = re.compile(clef)
+        res = p.finditer(txt)
+        df = pd.DataFrame(columns = ['Contexte gauche', 'Motif trouvé', 'Contexte droit'])
+        for r in res:
+            (i, j) = r.span()
+            df.loc[len(df.index)] = [txt[i-contexte:i],  txt[i:j], txt[j:j+contexte]]
+            print (f"Trouvé en pos {i} : {txt[i:j]}")        
+        return df
+    
+    # Singleton
+    @staticmethod
+    def getInstance(name, authors, id2doc):
+        if not Corpus.instance:
+            Corpus.instance = Corpus(name, authors, id2doc)
+        return Corpus.instance
+    
+    # Factory pattern
+    @staticmethod
+    def createInstance(name, authors, id2doc):
+        return Corpus(name, authors, id2doc)
+            
